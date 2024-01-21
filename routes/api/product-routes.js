7d02912d50aee72318577eaf2aec07bba344d6a9
AleSolis24 <aletitan24@icloud.com>
2024-01-21 18:03:39 -0500

    completed the products routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({include: [{model: Tag}],
+    })
+    res.json(products)
+  } catch (err) {
+    res.status(500).json ({message: 'Could not find Product'})
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try { 
+    const productId = await Product.findByPk(req.params.id,
+      {include: [{model: Tag}]})
+      res.status(200).json(productId)
+  } catch (err) {
+    res.status(500).json({message: 'There is a ERROR finding your ProductID!'});
+  }
 });
 
 // create new product
@@ -94,6 +108,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.delete(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  })
 });
 
 module.exports = router;
